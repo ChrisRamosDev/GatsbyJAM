e0884eb6d5a912684c1db9e3b8d11bd9a4215386
Chris Ramos <Cramos7114@gmail.com>
2020-07-27 11:44:15 -0400

    built gatsbyjs/cosmicjs blog site

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,49 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+const createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    {
+      allCosmicjsTrips(
+        sort: { fields: published_at, order: DESC }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  const trips = result.data.allCosmicjsTrips.edges
+  const tripsPerPage = 3
+  const numberOfPages = Math.ceil(trips.length / tripsPerPage)
+  Array.from({ length: numberOfPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/` : `/${i + 1}`,
+      component: path.resolve("./src/templates/post-list.js"),
+      context: {
+        limit: tripsPerPage,
+        skip: i * tripsPerPage,
+        numberOfPages,
+        currentPage: i + 1,
+      },
+    })
+  })
+
+  trips.forEach((trip, i) => {
+    createPage({
+      path: trip.node.slug,
+      component: path.resolve("./src/templates/post-page.js"),
+      context: {
+        slug: trip.node.slug,
+      },
+    })
+  })
+}
+
+module.exports = {
+  createPages,
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,4 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
-import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
@@ -26,26 +18,11 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+      <div className="container my-12 mx-auto px-4 md:px-12">
+        <main className="flex flex-wrap mx-1 lg:mx-4">{children}</main>
       </div>
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
